aa76eb12f2556c36fa81e5a4194525f72790f7f0
Christian Stigen Larsen <csl@sublevel3.org>
2015-04-07 22:01:20 +0200

    Bumps version

--- elv/__init__.py
+++ elv/__init__.py
@@ -9,8 +9,8 @@ from .elv import (
 __author__ = "Christian Stigen Larsen"
 __copyright__ = "Copyright (C) 2015 Christian Stigen Larsen"
 __email__ = "csl@csl.name"
-__license__ = "AGPL v3 or later"
-__version__ = "1.0.6"
+__license__ = "Affero GPL v3 or later"
+__version__ = "1.0.7"
 
 __all__ = [
     "Parse",
