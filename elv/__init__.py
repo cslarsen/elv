1834d414068e3a4ef2dc23aa3e5121be841aaa0b
Christian Stigen Larsen <csl@sublevel3.org>
2015-04-07 21:08:48 +0200

    Bumps version

--- elv/__init__.py
+++ elv/__init__.py
@@ -10,7 +10,7 @@ __author__ = "Christian Stigen Larsen"
 __copyright__ = "Copyright (C) 2015 Christian Stigen Larsen"
 __email__ = "csl@csl.name"
 __license__ = "AGPL v3 or later"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 __all__ = [
     "Parse",
